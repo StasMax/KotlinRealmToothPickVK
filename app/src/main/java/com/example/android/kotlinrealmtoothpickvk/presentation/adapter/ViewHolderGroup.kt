c622132a7ae4a0a74d55de2ddf131cb61c9f7e50
StasMax <maximovich@live.ru>
2019-05-06 12:42:41 +0300

    add syntetic in viewHolder

--- app/src/main/java/com/example/android/kotlinrealmtoothpickvk/presentation/adapter/ViewHolderGroup.kt
+++ app/src/main/java/com/example/android/kotlinrealmtoothpickvk/presentation/adapter/ViewHolderGroup.kt
@@ -2,24 +2,20 @@ package com.example.android.kotlinrealmtoothpickvk.presentation.adapter
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.CheckBox
-import android.widget.TextView
 import com.example.android.kotlinrealmtoothpickvk.R
 import com.example.android.kotlinrealmtoothpickvk.data.repository.ModelGroup
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.recycler.view.*
 
 class ViewHolderGroup(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    var civAvatar = itemView.findViewById<CircleImageView>(R.id.groups_siv_avatar)
-    var txtGroupName = itemView.findViewById<TextView>(R.id.group_txt_name)
-    var txtSubscribers = itemView.findViewById<TextView>(R.id.group_txt_subscribers)
-    var checkBox = itemView.findViewById<CheckBox>(R.id.isFavorite_checkBox)
+    var civAvatar: CircleImageView = itemView.findViewById(R.id.groups_siv_avatar)
 
     fun bind(groupModel: ModelGroup) {
-        txtGroupName.text = groupModel.name
-        txtSubscribers.text = groupModel.subscribers
-        checkBox.isChecked = groupModel.isFavorite
+        itemView.group_txt_name.text = groupModel.name
+        itemView.group_txt_subscribers.text = groupModel.subscribers
+        itemView.isFavorite_checkBox.isChecked = groupModel.isFavorite
         groupModel.avatar?.let { Picasso.with(itemView.context).load(it).into(civAvatar) }
     }
 }
